63dfa4c79806e4ba0c7d837fcde23577ca79dd2f
SaumyaPatil <saupatil10dec@gmail.com>
2025-02-11 19:58:38 +0530

    Javascript Execution Context

--- Revision/Javascript/test.js
+++ Revision/Javascript/test.js
@@ -1,7 +1,23 @@
+
+getName();
+getName2();
+
 var x = 10;
-function getName(){
+function getName(){                 //This behaves like a function
   console.log("Namaste JavaScript!");
+}
+
+var getName2 = () =>{               //This behaves like any other variable
+  console.log("Namaste JavaScript 2");
+}
+
+var getName3 = function(){          //This behaves like any other variable
+  console.log("Namaste JavaScript 3");
 }
 
 console.log(x);
 console.log(getName);
+console.log(getName2);
+
+getName();
+getName2();
---
+++ Revision/Javascript/test1.js
@@ -0,0 +1,21 @@
+
+
+//Function invocation and variable environment
+//The execution context is seperately running that is independant of each other. They have their own memory space, virtual environment where they run.
+
+var x = 10;
+a();
+b();
+
+console.log(x);
+
+function a(){
+    var x = 20;
+    console.log(x);
+}
+
+function b(){
+    var x = 30;
+    console.log(x);
+}
+
