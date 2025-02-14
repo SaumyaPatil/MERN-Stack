60bd9c38202611dd778982ed416d60650d101fcc
SaumyaPatil <saupatil10dec@gmail.com>
2025-02-15 00:18:35 +0530

    Questions on closures

--- Revision/Javascript/closures.js
+++ Revision/Javascript/closures.js
@@ -93,16 +93,141 @@
 // x();
 
 //Here you have to use var only and print from 1-5 in every 1 second
-function j(){
-    for(var i=1; i<=5; i++){
-        function caller(i){
-            setTimeout(function(){
-                console.log(i);
-            }, i*1000);
-        }
-        console.log(caller(i));
-    }
-    console.log("Namaste Javascript!");
-}
-j();
+// function j(){
+//     for(var i=1; i<=5; i++){
+//         function caller(i){
+//             setTimeout(function(){
+//                 console.log(i);
+//             }, i*1000);
+//         }
+//         console.log(caller(i));
+//     }
+//     console.log("Namaste Javascript!");
+// }
+// j();
 
+// function outer(){
+//     var a = 10;
+//     function inner(){
+//         console.log(a);
+//     }
+//     return inner;
+// }
+
+// outer()();
+//The inner function forms closure with its outer environment but not in any particular sequence or so. 
+//So, it forms closure.
+
+//Q. What if we use let instead of var?
+//Ans. It still forms closure. It behaves the same way.
+
+//Q. What if we add a parameter to the outer function and try logging it inside of inner function and return the inner function. 
+//Ans. It will work the same way as 'b' is a part of the outer environment of inner function.
+
+//Q. If we provide one more level of nesting it will still have access to the lexical environment of outest function.
+// function outest(b){
+//     var c = 20;
+//     function outer(b){
+//         function inner(){
+//             console.log(a, b, c);
+//         }
+//         let a = 30;
+//         return inner;
+//     }
+//     return outer;
+// }
+// var close = outest()("hello world!");
+// close();
+
+//Q. If we have a global variable also with conflicting name. What will happen?
+//It tries to resolve in the scope chain.
+// function outest(b){
+//     var c = 20;
+//     function outer(b){
+//         function inner(){
+//             console.log(a, b, c);
+//         }
+//         let a = 30;
+//         return inner;
+//     }
+//     return outer;
+// }
+// let a = 100;
+// var close = outest()("hello world!");
+// close();
+
+//Uses of closures - find the uses
+//It helps in data hiding and encapsulation
+//uses of closures
+//module design pattern
+//currying
+//functions like once
+//memoize
+//maintaining state in async world
+//setTimeouts
+//Iterators
+
+//Q. Uses of closures in data hiding and encapsulation
+// Data hiding means suppose we have a variable, and we want to have some data privicy over it so that other parts of code cannot have access to that particular data.
+
+//Yahaape kya hoga variables ka? - Yaha scope alag hai doono functions ke count ka.
+// function counter(){
+//     var count = 0;
+//     function incrementCounter(){
+//         count++;
+//         // console.log(count++);
+//         console.log(count);
+//         console.log(++count);
+//         console.log(count);
+//     }
+//     incrementCounter();
+//     count = 30;
+//     console.log(count);
+// }
+
+// var counter1 = counter();
+// counter1();
+// counter1();
+
+// var counter2 = counter();
+// counter2();
+// counter2();
+// counter2();
+// counter2();
+
+// counter();
+
+let i = 8; 
+while (i <= 10) {
+//   console.log(i);
+  k=10;
+  console.log(k);
+  i++;
+}
+// console.log(i);
+console.log(k);
+
+counter1.incrementCounter();
+counter1.incrementCounter();
+counter1.decrementCounter();
+
+//How to make the above code scalable by not just making it increment counter but also decrement counter.
+function Counter(){
+    var count=0;
+    this.incrementCounter = function(){
+        count++;
+        console.log(count);
+    }
+    this.decrementCounter = function(){
+        count++;
+        console.log(count);
+    }
+}
+
+var counter1 = new Counter();
+counter1.incrementCounter();
+counter1.decrementCounter();
+
+//Overconsumption of memory. Those closed over variables are not garbage collected till the program expires. 
+//If not handled properly it may lead to memory leaks.
+//The memory is accumulated overtime and may freez browser if not handled properly.
