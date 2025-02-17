5588e738c658c83427449010fea2ba516b29123a
SaumyaPatil <saupatil10dec@gmail.com>
2025-02-17 18:18:39 +0530

    Trust issues with setTimeout

--- Revision/Javascript/TrustIssues_of_setTimeout.js
+++ Revision/Javascript/TrustIssues_of_setTimeout.js
@@ -1,1 +1,14 @@
 //The JS thread is busy unless the whole code runs. It doesnt fetch anything from the queue otherwise.
+
+//You shouldnt have something in the callstack that is blocking the main thread.
+//Otherwise it wont be able to process any other events.
+
+//Lets block the main thread
+
+let startDate = new Date().getTime();
+let endDate = startDate;
+while(endDate < startDate+10000){
+    endDate = new Date().getTime();
+    console.log(endDate);
+}
+
