9fa96bc9a38f6adc56427f28e2b35e028f942015
SaumyaPatil <saupatil10dec@gmail.com>
2025-02-17 18:40:32 +0530

    Trust Issues with setTimeout

--- Revision/Javascript/TrustIssues_of_setTimeout.js
+++ Revision/Javascript/TrustIssues_of_setTimeout.js
@@ -5,10 +5,25 @@
 
 //Lets block the main thread
 
-let startDate = new Date().getTime();
-let endDate = startDate;
-while(endDate < startDate+10000){
-    endDate = new Date().getTime();
-    console.log(endDate);
-}
+// let startDate = new Date().getTime();
+// let endDate = startDate;
+// while(endDate < startDate+10000){
+//     endDate = new Date().getTime();
+//     console.log(endDate);
+// }
 
+//Why does JS has one call stack? It is interpretted language which makes it fast.
+//This kind of scenerio that we just mentioned above wont be seen in code anywhere. Why would anybody write setTimeout at the start of the code and wait for 5000 milliseconds. 
+//Its just to make you understand the concurrency model.
+
+//Whats up with settimeout for 0 seconds?
+
+console.log("Start");
+
+setTimeout(function cb(){
+    console.log("Callback");
+}, 0);
+
+console.log("End");
+//Suppose we want to differ our code, means that piece of code isnt that important.
+//Suppose its on fourth page or at end, we can do this.
