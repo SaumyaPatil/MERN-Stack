3a498e15faac512b4948b9f60eaafeb98693b9c8
SaumyaPatil <saupatil10dec@gmail.com>
2025-03-17 23:20:40 +0530

    Interview questions 1-20

---
+++ JavaScript_interviewBit/Q1.js
@@ -0,0 +1,28 @@
+// console.log(typeof "John Doe"); // Returns "string"
+// console.log(typeof 3.14); // Returns "number"
+// console.log(typeof true); // Returns "boolean"
+// console.log(typeof 234567890123456789012345678901234567890n); // Returns bigint
+// console.log(typeof undefined); // Returns "undefined"
+// console.log(typeof null); // Returns "object" (kind of a bug in JavaScript)
+// console.log(typeof Symbol('symbol')); // Returns Symbol
+
+
+// //Symbol data-type
+// var symbol1 = Symbol('symbol');
+// var symbol2 = Symbol('symbol');
+// console.log(symbol1 == symbol2);  //this gives false since the values are unique
+
+// Collection of data in key-value pairs
+var obj1 = {
+    x: 43,
+    y: "Hello world!",
+    z: function(){
+    return this.x;
+    }
+    }
+    // Collection of data as an ordered list
+    var array1 = [5, "Hello", true, 4.1];
+
+// console.log(obj1);
+
+console.log(obj1.z());
