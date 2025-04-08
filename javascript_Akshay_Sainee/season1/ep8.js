fd64c46bd06622b124fa9983928a00cf55e6c977
SaumyaPatil <saupatil10dec@gmail.com>
2025-04-08 07:06:06 +0530

    Let & const and temporal dead zone

---
+++ javascript_Akshay_Sainee/season1/ep8.js
@@ -0,0 +1,12 @@
+
+console.log(b);  //This is completely valid as memory is allocated to variables and functions even before a single line of code is executed!
+
+console.log(a);  //This throws error that it cannot be accessed before initialization. Meaning, it can only be assigned after you have initialized some value to it.
+
+//Is it hoisted. Is it allocated memory, will it behave like var
+
+let a = 10;   //Memory is allocated but they are stored in different memory space than global which cannot be accessed before you have put in some value in them.
+var b = 100;  //Memory is assigned to b and they are attached to the global object
+
+//TDZ - Its the time since the variable was hoisted till it is initialized some value
+
