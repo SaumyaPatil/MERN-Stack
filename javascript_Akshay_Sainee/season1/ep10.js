7a2293f07d8cb321a07cdc439b24f5a1b2e26ce7
SaumyaPatil <saupatil10dec@gmail.com>
2025-04-08 19:46:47 +0530

    Closures

--- javascript_Akshay_Sainee/season1/ep10.js
+++ javascript_Akshay_Sainee/season1/ep10.js
@@ -15,17 +15,56 @@
 
 //Lets learn closures by returning the function y
 
-function x(){
-    var a = 7;
-    function y(){
-        console.log(a);
-    }
-    return y;
-}
+// function x(){
+//     var a = 7;
+//     return function y(){
+//         console.log(a);
+//     }
+// }
 
-var z = x();  //Once this line is executed, x is no longer in call stack.
+// var z = x();  //Once this line is executed, x is no longer in call stack.
 //How does function y behaves in outside of x. Functions when returned still maintain their lexical scope.
 //When y was returned not just the function scope but closure was returned. It includes function along with its lexical scope.
 
-console.log(z);
+// console.log(z);
 
+//Corner cases in closures
+// function x(){
+//     var a = 7;
+//     function y(){
+//         console.log(a);
+//     }
+//     a = 100;   
+//     return y;
+// }
+
+// var z = x();
+// console.log(z);  //It prints 100 as a's reference is returned and not the value.
+//It means a was still in the memory preserved, when x was gone, it was not garbage collected.
+
+
+//What if we go one more level deep
+//Here it forms closure with a and b variables both. If I return the function y outside, it would retain the values of a and b.
+// function z(){
+//     var b = 100;
+//     function x(){
+//         var a = 7;
+//         function y(){
+//             console.log(a, b);
+//         }
+//         y();
+//     }
+//     x();
+// }
+
+// z();
+
+//  Uses of Closures
+//  Module Design Pattern
+//  Currying
+//  Functions like once - something inside the function remembers how many times it has been run.
+//  memoize
+//  maintaining state in async world
+//  setTimeouts
+//  Iterators
+//  and many more...
