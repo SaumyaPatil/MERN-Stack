4d720fb725cacb92912caa113b68804317b1da29
SaumyaPatil <saupatil10dec@gmail.com>
2025-04-08 07:22:08 +0530

    Temporal dead zone

--- javascript_Akshay_Sainee/season1/ep8.js
+++ javascript_Akshay_Sainee/season1/ep8.js
@@ -1,3 +1,4 @@
+//TDZ - Its the time since the variable was hoisted till it is initialized some value
 
 console.log(b);  //This is completely valid as memory is allocated to variables and functions even before a single line of code is executed!
 
@@ -8,13 +9,3 @@ console.log(a);  //This throws error that it cannot be accessed before initializ
 let a = 10;   //Memory is allocated but they are stored in different memory space than global which cannot be accessed before you have put in some value in them.
 var b = 100;  //Memory is assigned to b and they are attached to the global object
 
-//TDZ - Its the time since the variable was hoisted till it is initialized some value
-
-// priorities - 
-// 1. const (when you dont want to assign anything to the same variable)
-// 2. let (it has TDZ)
-// 3. Use var very conciously
-
-// To avoid temporal dead zone, put your declarations and initalisations at the top always.
-// Cause as the code starts running it hits the initialization part at first. Otherwise you can run into a lot of unexpected errors in JS.
-// By doing so you shrink the TDZ window to zero. Nothing happens before initialisation.
