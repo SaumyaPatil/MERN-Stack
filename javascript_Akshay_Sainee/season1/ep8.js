7a8153a4e4ce985783aab2148e64fc5d7812c884
SaumyaPatil <saupatil10dec@gmail.com>
2025-04-08 07:13:51 +0530

    TDZ

--- javascript_Akshay_Sainee/season1/ep8.js
+++ javascript_Akshay_Sainee/season1/ep8.js
@@ -10,3 +10,11 @@ var b = 100;  //Memory is assigned to b and they are attached to the global obje
 
 //TDZ - Its the time since the variable was hoisted till it is initialized some value
 
+// priorities - 
+// 1. const (when you dont want to assign anything to the same variable)
+// 2. let (it has TDZ)
+// 3. Use var very conciously
+
+// To avoid temporal dead zone, put your declarations and initalisations at the top always.
+// Cause as the code starts running it hits the initialization part at first. Otherwise you can run into a lot of unexpected errors in JS.
+// By doing so you shrink the TDZ window to zero. Nothing happens before initialisation.
