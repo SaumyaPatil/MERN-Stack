5e0cb0537b088b5ed51c60c8fc63d95fa63bc990
SaumyaPatil <saupatil10dec@gmail.com>
2025-04-09 10:26:44 +0530

    Closures interview questions

--- javascript_Akshay_Sainee/season1/ep11.js
+++ javascript_Akshay_Sainee/season1/ep11.js
@@ -12,7 +12,7 @@ x();
 //How to print 1 to 5 each after the number of seconds each
 
 // Wrong way - 
-// It remembers the reference to 'i'. Here it refers to the same memory location for every copy of the function. Each is refering to same location cause the environment for all of these functions are same. 
+// It remembers the reference to 'i'. Each copy of the function is refering to same location cause the environment for all of these functions are same. 
 // Since JS doesnt wait, till the timer expires, the value of i becomes 6 by the time the callback function gets chance to execute.
 function x(){
     for(var i=1; i<=5; i++){
@@ -30,7 +30,7 @@ x();
 
 
 //Fix using var only
-//Everytime you call this function z with i, it creates a copy of itself
+//Everytime you call this function z with i, the parameter excepts a new copy of 'i'. The code withen the function refers to that parameter.
 function x(){
     for(var i=1; i<=5; i++){
         function z(i){
