d1145841fa4c51ec9f1fd285e875fa91595c5a4f
SaumyaPatil <saupatil10dec@gmail.com>
2025-04-18 17:43:44 +0530

    This was all about lifting state up

---
+++ react_Love_Babbar/react-project-five/src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Card = (props) => {
+  return (
+    <div>
+      {/* Whatever changes you make here in the input field, those changes will be updated in the name state variable */}
+      <input type='text' onChange={(e)=>props.setName(e.target.value)}/>
+      <p>Name state variable inside {props.title} : {props.name}</p>
+    </div>
+  )
+}
+
+export default Card
